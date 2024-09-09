6b62197da33b138dde8705aecd100798097ac6dc
CamronBaker cool <cmbaker09@yahoo.com>
2024-09-09 12:57:14 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -72,7 +72,11 @@ $(document).ready(function () {
   }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat){
+    repeat = repeat++;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -85,12 +89,15 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    variable currentShape()
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.repeat, currentShape.shape)
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
